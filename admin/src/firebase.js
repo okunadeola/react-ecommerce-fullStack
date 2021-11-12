decfb29c5cb68fb946e6fd7f404b841b74aeaeeb
okunadeola <87997756+okunadeola@users.noreply.github.com>
2021-11-12 15:00:03 +0100

    Update firebase.js

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUIPJiY-IzkRieoOki8OZffImNHfJGs6o",
-  authDomain: "react-ecommerce-d5974.firebaseapp.com",
-  projectId: "react-ecommerce-d5974",
-  storageBucket: "react-ecommerce-d5974.appspot.com",
-  messagingSenderId: "595281195542",
-  appId: "1:595281195542:web:4aeb90a350e80c6bdefb37"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
